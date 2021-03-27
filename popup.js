252d12d1ff5f97135193d5b193bc4380ddb65342
Spencer Janelle <stj1006@wildcats.unh.edu>
2021-03-27 16:40:41 -0400

    auto false check done

--- popup.js
+++ popup.js
@@ -5,6 +5,28 @@ chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
 });
 
+
+links = document.getElementsByTagName('a');
+
+for (i = 0;  i < links.length; i++) { //for every link, create a mouseover event
+    links[i].onmouseover = function () {
+        //saves default style to oldStyle attribute
+        if (this.hasAttribute("oldStyle") == false){ //checks whether oldstyle attribute exists
+            var old = this.getAttribute("style");
+            this.setAttribute("oldStyle", old);
+        }
+        //console.log(this.href);
+        if (this.href != ""){
+
+
+          //This is where we would open the popup
+          this.title = "THIS HAS BEEN FLAGGED AS POTENTIAL MISINFORMATION!";
+
+
+        }
+    }
+}
+
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -15,28 +37,6 @@ changeColor.addEventListener("click", async () => {
   });
 });
 
-function enableRatingHover() {
-  links = document.getElementsByTagName('a');
-
-  for (i = 0;  i < links.length; i++) { //for every link, create a mouseover event
-      links[i].onmouseover = function () {
-          //saves default style to oldStyle attribute
-          if (this.hasAttribute("oldStyle") == false){ //checks whether oldstyle attribute exists
-              var old = this.getAttribute("style");
-              this.setAttribute("oldStyle", old);
-          }
-          //console.log(this.href);
-          if (this.href != ""){
-
-
-            //This is where we would open the popup
-            this.title = "THIS HAS BEEN FLAGGED AS POTENTIAL MISINFORMATION!";
-
-
-          }
-      }
-  }
-}
 
 // The body of this function will be execuetd as a content script inside the
 // current page
