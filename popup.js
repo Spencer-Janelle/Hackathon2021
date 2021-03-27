9fe77da6e6429e9df8476d77bde5a608c499d2e1
Spencer Janelle <stj1006@wildcats.unh.edu>
2021-03-27 16:14:17 -0400

    tinkering

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 // Initialize butotn with users's prefered color
-let changeColor = document.getElementById("changeColor");
+let changeColor = document.getElementById("enable");
 
 chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
@@ -11,9 +11,32 @@ changeColor.addEventListener("click", async () => {
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    function: enableRatingHover,
   });
 });
+
+function enableRatingHover() {
+  links = document.getElementsByTagName('a');
+
+  for (i = 0;  i < links.length; i++) { //for every link, create a mouseover event
+      links[i].onmouseover = function () {
+          //saves default style to oldStyle attribute
+          if (this.hasAttribute("oldStyle") == false){ //checks whether oldstyle attribute exists
+              var old = this.getAttribute("style");
+              this.setAttribute("oldStyle", old);
+          }
+          //console.log(this.href);
+          if (this.href != ""){
+
+
+            //This is where we would open the popup
+            this.title = "THIS HAS BEEN FLAGGED AS POTENTIAL MISINFORMATION!";
+
+
+          }
+      }
+  }
+}
 
 // The body of this function will be execuetd as a content script inside the
 // current page
