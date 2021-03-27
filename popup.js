4ba009e18b42c7a834f4dfe0b3a2bb3fed0c1faf
Spencer Janelle <stj1006@wildcats.unh.edu>
2021-03-27 16:59:06 -0400

    added info checking for 5g

--- popup.js
+++ popup.js
@@ -37,6 +37,32 @@ changeColor.addEventListener("click", async () => {
   });
 });
 
+function enableRatingHover() {
+  links = document.getElementsByTagName('a');
+
+  for (i = 0;  i < links.length; i++) { //for every link, create a mouseover event
+      links[i].onmouseover = function () {
+          //saves default style to oldStyle attribute
+          if (this.hasAttribute("oldStyle") == false){ //checks whether oldstyle attribute exists
+              var old = this.getAttribute("style");
+              this.setAttribute("oldStyle", old);
+          }
+          //console.log(this.href);
+          if (this.href != ""){
+
+            test = this.href.toLowerCase();
+            //This is where we would open the popup
+            if((test.includes("5g") && (test.includes("coronavirus")))) {
+            this.title = "THIS HAS BEEN FLAGGED AS POTENTIAL MISINFORMATION!";
+          } else {
+          this.title = "NO MISINFORMATION DETECTED!";
+          }
+
+
+          }
+      }
+  }
+}
 
 // The body of this function will be execuetd as a content script inside the
 // current page
