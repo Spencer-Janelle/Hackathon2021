bb7026f4f12a11be11d49b6db02c3edb51fd79dc
Spencer Janelle <stj1006@wildcats.unh.edu>
2021-03-27 23:55:13 -0400

    nonsense

--- popup.js
+++ popup.js
@@ -1,1 +1,23 @@
 
+// Initialize butotn with users's prefered color
+let buttons = document.getElementById("showOrHide");
+
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+buttons.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setVisibility,
+  });
+});
+
+// The body of this function will be execuetd as a content script inside the
+// current page
+function setVisibility() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+  buttons.style.display = "none";
+}
