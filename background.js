d0ff44b73f47053aac536362da73ed1e39a3a7ad
Spencer Janelle <stj1006@wildcats.unh.edu>
2021-03-27 20:42:38 -0400

    added json file, trying to log it

--- background.js
+++ background.js
@@ -3,3 +3,9 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+fetch("./data.json")
+.then(response => {
+   return response.json();
+})
+.then(data => console.log(data));
