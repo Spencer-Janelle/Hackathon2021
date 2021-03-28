791df5388586291d56adb6952e07f28767df6ef3
Cornelis Plomp <cplomp@iol.unh.edu>
2021-03-27 20:04:31 -0400

    Switched from title to popup trigger

--- scanner.js
+++ scanner.js
@@ -1,3 +1,8 @@
+// Test bubble
+var bubbleDOM = document.createElement('div');
+bubbleDOM.setAttribute('class', 'selection_bubble');
+document.body.appendChild(bubbleDOM);
+
 // Mouse listener for any move event on the current document.
 
 document.addEventListener('mousemove', function (e) {
@@ -10,9 +15,15 @@ document.addEventListener('mousemove', function (e) {
         test = srcElement.href.toLowerCase();
         //This is where we would open the popup
         if ((test.includes("5g") && (test.includes("coronavirus")))) {
-            srcElement.title = "THIS HAS BEEN FLAGGED AS POTENTIAL MISINFORMATION!";
+            renderBubble(e.clientX, e.clientY, "THIS HAS BEEN FLAGGED AS POTENTIAL MISINFORMATION!")
         } else {
-            srcElement.title = "NO MISINFORMATION DETECTED!";
+            renderBubble(e.clientX, e.clientY, "NO MISINFORMATION DETECTED")
         }
+    }
+    function renderBubble(mouseX, mouseY, srcElement) {
+        bubbleDOM.innerHTML = srcElement;
+        bubbleDOM.style.top = mouseY + 'px';
+        bubbleDOM.style.left = mouseX + 'px';
+        bubbleDOM.style.visibility = 'visible';
     }
 });
